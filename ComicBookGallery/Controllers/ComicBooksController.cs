bed43aa1a908b5d401f96f6ef1258ebd3b0d2c09
Britton Kitchell <brittonashford@gmail.com>
2021-09-06 15:07:42 -0500

    updated comic book detail view

--- ComicBookGallery/Controllers/ComicBooksController.cs
+++ ComicBookGallery/Controllers/ComicBooksController.cs
@@ -10,6 +10,18 @@ namespace ComicBookGallery.Controllers
     {
         public ActionResult Detail()
         {
+            ViewBag.SeriesTitle = "The Amazing Spider-Man";
+            ViewBag.IssueNumber = 700;
+            ViewBag.Description = "<p>Final issue! Witness the final hours of Doctor Octopus' life and his one, last, great act of revenge! Even if Spider-Man survives... <strong>will Peter Parker?</strong></p>";
+            ViewBag.Artists = new string[]
+            {
+                "Script: Dan Slott",
+                "Pencils: Humberto Ramos",
+                "Inks: Victor Olazaba",
+                "Colors: Edgar Delgado",
+                "Letters: Chris Eliopoulos"
+            };
+
             return View();
         }
     }
@@ -23,3 +35,6 @@ namespace ComicBookGallery.Controllers
 
 //NOTES:
 //Controller base class' View() method returns a ViewResult object from an action method
+
+//Viewbag is an MVC object that lets you pass data from Controller to View
+//    define properties in Controller, access them in the view
